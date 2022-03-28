237ea4e3acc69959506c42535937c70332540bb6
Emily <71280699+Emilyoftg@users.noreply.github.com>
2022-03-28 10:38:11 +0530

    Nathalia Loading..

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/Ajax.git /Ajax
+  git clone https://github.com/Emilyoftg/Extra-1.0.git /Extra-1.0
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Ajax
+  git clone $UPSTREAM_REPO /Extra-1.0
 fi
 cd /Ajax
 pip3 install -U -r requirements.txt
-echo "Starting ᗩᒍᗩ᙭....🔥"
+echo "Starting Nathalia....🔥"
 python3 bot.py
